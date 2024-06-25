bc9f04c547c5316dc4c3c34aeb6ca8b8359153d4
AmableAAristy <chimchar1205@gmail.com>
2024-06-24 22:33:30 -0400

    testing functionality

---
+++ GroupProject/GroupProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GroupProject
+//
+//  Created by Amable A Aristy  on 6/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GroupProject/GroupProject/GroupProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupProjectApp.swift
+//  GroupProject
+//
+//  Created by Amable A Aristy  on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct GroupProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
