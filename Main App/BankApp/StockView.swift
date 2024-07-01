cabf4f1ad95f3b730324611b15bff0632a3c0780
AmableAAristy <chimchar1205@gmail.com>
2024-06-30 23:16:10 -0400

    created stock and stockview()

---
+++ Main App/BankApp/StockView.swift
@@ -0,0 +1,18 @@
+//
+//  StockView.swift
+//  BankApp
+//
+//  Created by Amable A Aristy  on 6/30/24.
+//
+
+import SwiftUI
+
+struct StockView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StockView()
+}
