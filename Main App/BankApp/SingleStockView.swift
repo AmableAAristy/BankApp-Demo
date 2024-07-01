9cfeac5f1e8671a59acc24135dc3ab9492ec41fa
AmableAAristy <chimchar1205@gmail.com>
2024-07-01 11:03:02 -0400

    set up the flask server

---
+++ Main App/BankApp/SingleStockView.swift
@@ -0,0 +1,18 @@
+//
+//  SingleStockView.swift
+//  BankApp
+//
+//  Created by Amable A Aristy  on 7/1/24.
+//
+
+import SwiftUI
+
+struct SingleStockView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SingleStockView()
+}
