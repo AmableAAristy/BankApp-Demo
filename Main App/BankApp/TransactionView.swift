41f2fe661633429de68630d28e866c5ebc929991
Angie Martinez <amart1070@fiu.edu>
2024-07-06 18:07:03 -0400

    Updated TransactionView

--- Main App/BankApp/TransactionView.swift
+++ Main App/BankApp/TransactionView.swift
@@ -13,6 +13,7 @@ struct TransactionView: View {
     @State var company: String = ""
     @State var costString: String = ""
     @State private var transactions: [Transaction] = []
+    
     var cost: Double {
         get {
             Double(costString) ?? 0.00
@@ -25,38 +26,142 @@ struct TransactionView: View {
     let db = Firestore.firestore()
     
     var body: some View {
-        VStack{
-            
-            TextField("Who did you do the transaction with?", text: $company)
-                .padding()
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            TextField("For how much?", text: $costString)
-                .keyboardType(.decimalPad)
-                .padding()
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            Button(action: {
-                addTransaction(company: company, price: cost){
-                    // Clear the input fields
-                    company = ""
-                    costString = ""
-                    fetchTransactions()
-                }
-            }) {
-                Text("Submit to database")
-            }
-            .padding()
-            List(transactions) { transaction in
-                HStack {
-                    Text(transaction.company)
+        NavigationStack{
+            VStack{
+                HStack{
                     Spacer()
-                    Text(String(format: "$%.2f", transaction.price))
-                        .foregroundColor(transaction.price <= 0.00 ? .red : .green)
+                    Text("Credit Card")
+                        .font(.headline).bold()
+                    Spacer()
+                }//end Hstack (header)
+                .padding(.horizontal, 20)
+                
+                // credit card image
+                Image("creditCard4")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 330, height: 200)
+                
+                // Actions section
+                HStack(spacing: 30) {
+                    VStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color.blue)
+                                .frame(width: 48, height: 48)
+                            
+                            Image(systemName: "arrow.down.to.line")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 24, height: 24)
+                                .foregroundColor(.white)
+                        }
+                        
+                        Text("Deposit")
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                    }
+                    
+                    VStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color.blue)
+                                .frame(width: 48, height: 48)
+                            
+                            Image(systemName: "dollarsign")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 24, height: 24)
+                                .foregroundColor(.white)
+                        }//end zstack
+                        
+                        Text("Pay")
+                            .font(.caption)
+                            .foregroundStyle(.gray)
+                    }//end vstack
+                    
+                    VStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color.blue)
+                                .frame(width: 48, height: 48)
+                            
+                            Image(systemName: "creditcard.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 24, height: 24)
+                                .foregroundColor(.white)
+                        }//end ZStack
+                        
+                        Text("More")
+                            .font(.caption)
+                            .foregroundStyle(.gray)
+                    }//end VStack
+                }//end HStack
+                .padding()
+                
+                Spacer()
+                
+                // ----------------------------------------------------
+                
+                // keep this to fix it later and make it pretty
+                TextField("Who did you do the transaction with?", text: $company)
+                    .padding()
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                TextField("For how much?", text: $costString)
+                    .keyboardType(.decimalPad)
+                    .padding()
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                Button(action: {
+                    addTransaction(company: company, price: cost){
+                        // Clear the input fields
+                        company = ""
+                        costString = ""
+                        fetchTransactions()
+                    }
+                }) {
+                    Text("Submit Amount")
                 }
-            }
-            .padding()
-            .onAppear(perform: fetchTransactions)
-        }
-    }//end Vstack
+                .padding()
+                
+                // -------------------------------------------------------
+                
+                // transactions list
+                List{
+                    Section(header: Text("Transactions")){
+                        ForEach(transactions) { transaction in
+                            HStack{
+                                
+                                Image(systemName: transaction.price <= 0.00 ? "arrow.down" : "arrow.up")
+                                    .foregroundStyle(transaction.price <= 0.00 ? .red : .green)
+                                
+                                VStack(alignment: .leading){
+                                    Text(Date().formatted())
+                                        .foregroundStyle(.secondary)
+                                        .font(.system(size: 12))
+                                    Text(transaction.company)
+                                        .font(.headline)
+                                }
+                            
+                                Spacer()
+                                
+                                VStack(alignment: .leading){
+                                    Text(String(format: "$%.2f", transaction.price))
+                                        .foregroundColor(transaction.price <= 0.00 ? .red : .green)
+                                }
+                            }//end HStack
+                        }//end ForEach
+                    }//
+                }//end List
+                .listStyle(InsetGroupedListStyle())
+                .padding()
+                .onAppear(perform: fetchTransactions)
+                
+            }//end Vstack
+        }//end Nav Stack
+        
+    }//end body
     
     func addTransaction(company: String, price: Double, completion: @escaping () -> Void) {
         
@@ -93,7 +198,7 @@ struct TransactionView: View {
             }
         }
     }//end fetch
-}
+}//end TransactionView
 
 
 
