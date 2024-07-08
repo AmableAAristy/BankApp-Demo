84699376038d77cbde6425fe522ffc382c4a8362
Angie Martinez <amart1070@fiu.edu>
2024-07-08 19:01:57 -0400

    updated transaction view

---
+++ Main App/BankApp/TransactionInputView.swift
@@ -0,0 +1,54 @@
+//
+//  TransactionInputView.swift
+//  BankApp
+//
+//  Created by Angie Martinez on 7/8/24.
+//
+
+import SwiftUI
+
+struct TransactionInputView: View {
+    @Binding var company: String
+    @Binding var costString: String
+    var addTransaction: (String, Double) -> Void
+    @Environment(\.presentationMode) var presentationMode
+
+    var body: some View {
+        VStack {
+            TextField("Who did you do the transaction with?", text: $company)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            TextField("For how much?", text: $costString)
+                .keyboardType(.decimalPad)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+
+            Button(action: {
+                if let cost = Double(costString) {
+                    addTransaction(company, cost)
+                    // Clear the input fields
+                    company = ""
+                    costString = ""
+                    presentationMode.wrappedValue.dismiss()
+                }
+            }) {
+                Text("Submit Amount")
+            }
+            .padding()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    @State var company = ""
+    @State var costString = ""
+    return VStack {
+        TransactionInputView(company: $company, costString: $costString) { company, price in
+            // Sample addTransaction function for preview
+            print("Adding transaction with company: \(company), price: \(price)")
+        }
+    }
+}
+
