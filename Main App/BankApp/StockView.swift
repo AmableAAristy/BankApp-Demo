4e4e854034bd592d5e270e00ee030902fc907570
AmableAAristy <chimchar1205@gmail.com>
2024-07-12 16:33:58 -0400

    Fixed logout and bug with stock

--- Main App/BankApp/StockView.swift
+++ Main App/BankApp/StockView.swift
@@ -1,17 +1,8 @@
-//
-//  StockView.swift
-//  BankApp
-//
-//  Created by Amable A Aristy  on 6/30/24.
-//
-
 import SwiftUI
 
 struct StockView: View {
-    
     @State private var stockName: String = ""
     @State private var suggestions: [String] = []
-    @Environment(\.presentationMode) var presentationMode
     
     private let trie = Trie()
     
@@ -23,57 +14,45 @@ struct StockView: View {
     }
     
     var body: some View {
-        
-        NavigationStack{
+        VStack {
+            Image("Stock")
+                .resizable()
+                .aspectRatio(contentMode:.fit)
+                .frame(height: 550)
             VStack {
-                Image ("Stock")
-                Spacer()
-                HStack{
-                    
-                    VStack{
-                        
-                        if !suggestions.isEmpty {
-                            List(suggestions, id: \.self) { suggestion in
-                                Text(suggestion)
-                                    .onTapGesture {
-                                        stockName = suggestion
-                                        suggestions.removeAll()
-                                        presentationMode.wrappedValue.dismiss()
-                                    }
-                            }
-                            .frame(height: 200)
-                            
-                        }
-                        
-                        TextField("Search for symbols!", text: Binding(
-                            get: {
-                                self.stockName
-                            },
-                            set: { newValue in
-                                self.stockName = newValue.uppercased().filter { $0.isUppercase }
-                                self.updateSuggestions()
-                            }
-                        ))
-                        .padding()
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .onChange(of: stockName) { newValue in
-                            updateSuggestions()
-                        }
-                        .padding(.horizontal)
-                        
-                        
-                    }//end vstack
-                    NavigationLink(destination: SingleStockView(stockName: stockName)){
-                        Text("Search")
-                    }
-                    
-                    
-                }//end Hstack
-                Spacer()
+                TextField("Search for symbols!", text: Binding(get: {
+                    self.stockName},
+                    set: {newValue in
+                    self.stockName = newValue.uppercased().filter {$0.isUppercase}
+                    self.updateSuggestions()
+                }
+                                                              ))
                 .padding()
-            }//endVstack
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .onChange(of: stockName) { newValue in
+                    self.updateSuggestions()
+                }
+                .padding(.horizontal)
+                if suggestions.isEmpty {
+                    EmptyView()
+                } else {
+                    List(suggestions, id: \.self) { suggestion in
+                        Text(suggestion)
+                            .onTapGesture {
+                                stockName = suggestion
+                                suggestions.removeAll()
+                            }
+                    }
+                    .frame(height: 200)
+                }
+            }
+            NavigationLink(destination: SingleStockView(stockName: stockName)) {
+                Text("Search")
+            }
         }
+        .padding()
     }
+    
     private func updateSuggestions() {
         if stockName.isEmpty {
             suggestions.removeAll()
@@ -83,6 +62,8 @@ struct StockView: View {
     }
 }
 
-#Preview {
-    StockView()
+struct StockView_Previews: PreviewProvider {
+    static var previews: some View {
+        StockView()
+    }
 }
